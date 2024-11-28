c58f8a476157552a35149d6dd3c67eaeba3a64c7
Marcio Pezzi <marciopezzi92@gmail.com>
2024-11-28 20:40:56 -0300

    feat: configure TypeORM and setup development environment
    - Added TypeORM configuration with PostgreSQL
    - Created a modular structure for database connection
    - Setup `typeorm.config.ts` for environment-based configuration
    - Integrated `.env` for managing environment variables
    - Verified the database connection and basic entity creation
    - Added docker-compose file for development with PostgreSQL and pgAdmin
    - Confirmed pgAdmin and CLI access to database

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => typeOrmConfig(configService),
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: parseInt(configService.get('DB_PORT'), 10),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [__dirname + '/../**/*.entity.{ts,js}'],
+  synchronize: true,
+});
